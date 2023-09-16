ea08f0625b5c6a243fce266e1ae01fed69fe6e03
Igors Koptevs <igors.koptevs@gmail.com>
2023-09-16 14:31:30 +0300

    Added Material UI

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,21 +1,37 @@
-import './bootstrap';
-import '../css/app.css';
+import "./bootstrap";
+import "../css/app.css";
 
-import { createRoot } from 'react-dom/client';
-import { createInertiaApp } from '@inertiajs/react';
-import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import * as React from "react";
+import { createRoot } from "react-dom/client";
+import { createInertiaApp } from "@inertiajs/react";
+import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
 
-const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "./theme";
+
+const appName = import.meta.env.VITE_APP_NAME || "Laravel";
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
-    resolve: (name) => resolvePageComponent(`./Pages/${name}.jsx`, import.meta.glob('./Pages/**/*.jsx')),
+    resolve: (name) =>
+        resolvePageComponent(
+            `./Pages/${name}.jsx`,
+            import.meta.glob("./Pages/**/*.jsx")
+        ),
     setup({ el, App, props }) {
         const root = createRoot(el);
 
-        root.render(<App {...props} />);
+        root.render(
+            <React.StrictMode>
+                <ThemeProvider theme={theme}>
+                    <CssBaseline />
+                    <App {...props} />
+                </ThemeProvider>
+            </React.StrictMode>
+        );
     },
     progress: {
-        color: '#4B5563',
+        color: "#4B5563",
     },
 });
