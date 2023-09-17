2eb333c29033f35759f83b94b6365fd861124437
Igors Koptevs <igors.koptevs@gmail.com>
2023-09-17 19:48:14 +0300

    Changed default font

--- resources/js/theme.js
+++ resources/js/theme.js
@@ -3,6 +3,29 @@ import { red } from "@mui/material/colors";
 
 // Create a theme instance.
 const theme = createTheme({
+    typography: {
+        poster: {
+            fontSize: "4rem",
+            fontWeight: "bold",
+            color: "red",
+            fontFamily: "tahoma",
+        },
+        // fontSize: 12,
+        fontFamily: [
+            "IBM Plex Sans",
+            "Montserrat",
+            '"Segoe UI Symbol"',
+            '"Apple Color Emoji"',
+            "Arial",
+            "Roboto",
+            "BlinkMacSystemFont",
+            '"Segoe UI"',
+            "-apple-system",
+            '"Helvetica Neue"',
+            "sans-serif",
+            '"Segoe UI Emoji"',
+        ].join(","),
+    },
     palette: {
         primary: {
             main: "#556cd6",
