5744b6e87bdd6c0bfcb5b520b54278a1cddb055b
Igors Koptevs <igors.koptevs@gmail.com>
2023-09-22 14:15:16 +0300

    Edit

--- resources/js/Pages/AdminPanel/Main.jsx
+++ resources/js/Pages/AdminPanel/Main.jsx
@@ -1,29 +1,141 @@
 import React from "react";
 import Typography from "@mui/material/Typography";
 
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Stack from "@mui/material/Stack";
+import TextField from "@mui/material/TextField";
+
+import { useForm } from "react-hook-form";
+import { DevTool } from "@hookform/devtools";
+
 import Layout from "./Layout";
 const Main = () => {
+    const form = useForm({
+        defaultValues: {
+            username: "",
+            email: "",
+            password: "",
+        },
+        // tutorial lesson 12
+        // defaultValues: async () => {
+        //     const response = await fetch(
+        //         "https://jsonplaceholder.typicode.com/users/2"
+        //     );
+        //     const data = await response.json();
+
+        //     return {
+        //         username: "Batman",
+        //         email: data.email,
+        //         password: "",
+        //     };
+        // },
+    });
+    const { register, control, handleSubmit, formState } = form;
+    const { errors } = formState;
+
+    const onSubmit = (data) => {
+        console.log("Form submitted", data);
+    };
+
     return (
         <>
             <Layout>
                 <Typography variant="h3">Main</Typography>
 
-                <Typography paragraph>
-                    Consequat mauris nunc congue nisi vitae suscipit. Fringilla
-                    est ullamcorper eget nulla facilisi etiam dignissim diam.
-                    Pulvinar elementum integer enim neque volutpat ac tincidunt.
-                    Ornare suspendisse sed nisi lacus sed viverra tellus. Purus
-                    sit amet volutpat consequat mauris. Elementum eu facilisis
-                    sed odio morbi. Euismod lacinia at quis risus sed vulputate
-                    odio. Morbi tincidunt ornare massa eget egestas purus
-                    viverra accumsan in. In hendrerit gravida rutrum quisque non
-                    tellus orci ac. Pellentesque nec nam aliquam sem et tortor.
-                    Habitant morbi tristique senectus et. Adipiscing elit duis
-                    tristique sollicitudin nibh sit. Ornare aenean euismod
-                    elementum nisi quis eleifend. Commodo viverra maecenas
-                    accumsan lacus vel facilisis. Nulla posuere sollicitudin
-                    aliquam ultrices sagittis orci a.
-                </Typography>
+                <Box
+                    component="form"
+                    sx={{
+                        "& .MuiTextField-root": {
+                            m: 1,
+                            // width: "25ch",
+                        },
+                    }}
+                    noValidate
+                    onSubmit={handleSubmit(onSubmit)}
+                    autoComplete="off"
+                >
+                    <TextField
+                        size="small"
+                        label="Username"
+                        type="text"
+                        {...register("username", {
+                            required: {
+                                value: true,
+                                message: "Username is required.",
+                            },
+                        })}
+                    />
+                    <p style={{ color: "red" }}>{errors.username?.message}</p>
+                    <TextField
+                        size="small"
+                        label="Email"
+                        // defaultValue="4CL"
+                        // helperText="Helper text"
+                        // fullWidth
+                        // multiline
+                        // rows={5}
+                        {...register("email", {
+                            pattern: {
+                                value: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+                                message: "Invalid EMail",
+                            },
+                            validate: {
+                                notAdmin: (fieldValue) => {
+                                    return (
+                                        fieldValue !== "admin@example.com" ||
+                                        `${fieldValue} is reserved for inernal usage!`
+                                    );
+                                },
+                                notBlackListed: (fieldValue) => {
+                                    return (
+                                        !fieldValue.endsWith("baddomain.com") ||
+                                        `Sorry, but ${fieldValue} is in our blacklist.`
+                                    );
+                                },
+                            },
+                        })}
+                    />
+                    <p style={{ color: "red" }}>{errors.email?.message}</p>
+                    <TextField
+                        size="small"
+                        label="Password"
+                        type="password"
+                        autoComplete="current-password"
+                        {...register("password")}
+                    />
+                    <div>
+                        <Button
+                            type="submit"
+                            variant="contained"
+                            color="primary"
+                            className="mt-4 ml-2"
+                        >
+                            Login
+                        </Button>
+                    </div>
+                    {/* <TextField
+                            required
+                            id="outlined-required"
+                            // label="Required"
+                            defaultValue="Hello World"
+                            sx={{
+                                width: "30%",
+                                "& .MuiInputBase-input": {
+                                    padding: "5px 17px",
+                                },
+                                "& .MuiOutlinedInput-root": {
+                                    color: "white",
+                                    backgroundColor: "tomato",
+                                    // padding: "1px",
+                                },
+                                "& .MuiInputLabel-root": {
+                                    color: "navy",
+                                },
+                            }}
+                        /> */}
+                    <DevTool control={control} />
+                </Box>
             </Layout>
             {/* <div>AdminPanel</div>; */}
         </>
