a8986ea8f13c09dd635e627a8120ee5848378099
Igors Koptevs <igors.koptevs@gmail.com>
2023-09-23 17:10:43 +0300

    Login form completed

--- resources/js/Pages/Auth/Login.jsx
+++ resources/js/Pages/Auth/Login.jsx
@@ -24,12 +24,6 @@ import { useForm } from "react-hook-form";
 import { DevTool } from "@hookform/devtools";
 
 export default function Login({ status, canResetPassword }) {
-    // const { data, setData, post, processing, errors, reset } = inertiaUseForm({
-    //     email: "",
-    //     password: "",
-    //     remember: false,
-    // });
-
     const form = useForm({
         defaultValues: {
             email: "",
@@ -43,21 +37,8 @@ export default function Login({ status, canResetPassword }) {
     const { errors: inertiaErrors } = usePage().props;
 
     const onSubmit = (data) => {
-        // console.log("Form submitted", data);
         router.post(route("login"), data);
     };
-
-    // useEffect(() => {
-    //     return () => {
-    //         reset("password");
-    //     };
-    // }, []);
-
-    // const submit = (e) => {
-    //     e.preventDefault();
-
-    //     post(route("login"));
-    // };
 
     return (
         <GuestLayout>
@@ -167,71 +148,6 @@ export default function Login({ status, canResetPassword }) {
                 </Stack>
             </Box>
             {/* <DevTool control={control} /> */}
-
-            {/* <form onSubmit={submit}>
-                <div>
-                    <InputLabel htmlFor="email" value="Email" />
-
-                    <TextInput
-                        id="email"
-                        type="email"
-                        name="email"
-                        value={data.email}
-                        className="mt-1 block w-full"
-                        autoComplete="username"
-                        isFocused={true}
-                        onChange={(e) => setData("email", e.target.value)}
-                    />
-
-                    <InputError message={errors.email} className="mt-2" />
-                </div>
-
-                <div className="mt-4">
-                    <InputLabel htmlFor="password" value="Password" />
-
-                    <TextInput
-                        id="password"
-                        type="password"
-                        name="password"
-                        value={data.password}
-                        className="mt-1 block w-full"
-                        autoComplete="current-password"
-                        onChange={(e) => setData("password", e.target.value)}
-                    />
-
-                    <InputError message={errors.password} className="mt-2" />
-                </div>
-
-                <div className="block mt-4">
-                    <label className="flex items-center">
-                        <Checkbox
-                            name="remember"
-                            checked={data.remember}
-                            onChange={(e) =>
-                                setData("remember", e.target.checked)
-                            }
-                        />
-                        <span className="ml-2 text-sm text-gray-600 dark:text-gray-400">
-                            Remember me
-                        </span>
-                    </label>
-                </div>
-
-                <div className="flex items-center justify-end mt-4">
-                    {canResetPassword && (
-                        <Link
-                            href={route("password.request")}
-                            className="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800"
-                        >
-                            Forgot your password?
-                        </Link>
-                    )}
-
-                    <PrimaryButton className="ml-4" disabled={processing}>
-                        Log in
-                    </PrimaryButton>
-                </div>
-            </form> */}
         </GuestLayout>
     );
 }
