38da456110d626446ff09042cb00c3bb56869958
Igors Koptevs <igors.koptevs@gmail.com>
2023-10-05 16:36:09 +0300

    LiftManager -a model created

---
+++ app/Http/Controllers/LiftManagerController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\LiftManager;
+use App\Http\Requests\StoreLiftManagerRequest;
+use App\Http\Requests\UpdateLiftManagerRequest;
+
+class LiftManagerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreLiftManagerRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(LiftManager $liftManager)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(LiftManager $liftManager)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateLiftManagerRequest $request, LiftManager $liftManager)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(LiftManager $liftManager)
+    {
+        //
+    }
+}
