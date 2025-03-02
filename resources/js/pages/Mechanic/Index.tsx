eddeca146ae5d7e8a516b50a8f8955f9709dae1b
Igors Koptevs <igors.koptevs@gmail.com>
2025-03-02 22:22:16 +0200

    Added layout to mechanics page

--- resources/js/pages/Mechanic/Index.tsx
+++ resources/js/pages/Mechanic/Index.tsx
@@ -1,22 +1,34 @@
+import AppLayout from '@/layouts/app-layout';
+import { type BreadcrumbItem } from '@/types';
+import { Head } from '@inertiajs/react';
 import { Mechanic } from '@/types';
 import React from 'react';
-import AuthenticatedLayout from '@/layouts/AuthenticatedLayout';
+
+const breadcrumbs: BreadcrumbItem[] = [
+    {
+        title: 'Mechanics',
+        href: '/mechanics',
+    },
+];
 
 const Index = ({ mechanics }: { mechanics: Mechanic[] }) => {
     return (
-        <AuthenticatedLayout header={<h2 className="text-xl leading-tight font-semibold text-gray-800 dark:text-gray-200">Mechanic Index</h2>}>
+        <AppLayout breadcrumbs={breadcrumbs}>
+            <Head title="Mechanics" />
             <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-                {mechanics.map((mech) => {
-                    return (
-                        <div key={mech.id} className="mb-2">
-                            <h2 className="font-semibold">{mech.name}</h2>
-                            <p className="text-xs font-bold">{mech.company}</p>
-                            <p>{mech.phone}</p>
-                        </div>
-                    );
-                })}
+                <div className="mt-4 grid gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-6">
+                    {mechanics.map((mech) => {
+                        return (
+                            <div key={mech.id} className="mb-2">
+                                <h2 className="font-semibold">{mech.name}</h2>
+                                <p className="text-xs font-bold">{mech.company}</p>
+                                <p>{mech.phone}</p>
+                            </div>
+                        );
+                    })}
+                </div>
             </div>
-        </AuthenticatedLayout>
+        </AppLayout>
     );
 };
 
